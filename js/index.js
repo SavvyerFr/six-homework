5494499a7c1cca783f4e19eacfc78cc4fe6267c5
Dmytro <fokshei.d@gmail.com>
2016-11-07 09:50:47 +0000

    q

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Java Script
+// var year = Number(prompt('Input your year'));
+// var month = Number(prompt('Input your month'));
+// var day = Number(prompt('Input your day'));
+// var sum = add(year, month, day);
+// var text = 'Your magic number';
+// var result = text + sum;
+// document.write(result);
